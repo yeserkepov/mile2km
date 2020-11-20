3e575eee992b06585ace35956a0aedf87aa202a5
Yesserkepov <yeserkepov@gmail.com>
2020-11-20 14:13:19 +0600

    Initial Commit

---
+++ mile2kmTests/mile2kmTests.swift
@@ -0,0 +1,33 @@
+//
+//  mile2kmTests.swift
+//  mile2kmTests
+//
+//  Created by User on 11/20/20.
+//
+
+import XCTest
+@testable import mile2km
+
+class mile2kmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
